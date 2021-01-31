5452d719fa443bc4257a2c5a2e96eff272abf11f
nhippert <nhippert@gmail.com>
2021-02-01 00:39:58 +0100

    LEDs+Ultrasoniic sensor support+Code fixes

--- Software/POC/POC.ino
+++ Software/POC/POC.ino
@@ -1,5 +1,3 @@
-
-
 /*ComplimentGenerator
    POC
    by Nicolas Hippert
@@ -12,31 +10,38 @@
 #include <Adafruit_Thermal.h>
 
 //Pins
-const int rs = 12;
-const int enable = 11;
-const int d4 = 5;
-const int d5 = 4;
-const int d6 = 3;
-const int d7 = 2;
-const int buttonPin = 7;
-const int buzzer = 8;
-#define TX_PIN 1 // Arduino transmit  YELLOW WIRE  labeled RX on printer
-#define RX_PIN 0 // Arduino receive   GREEN WIRE   labeled TX on printer
+const int rs = 12;        //Screen rs pin
+const int enable = 11;    //Screen enable pin
+const int d4 = 5;         //Screen d4 pin
+const int d5 = 4;         //Screen d5 pin
+const int d6 = 3;         //Screen d6 pin
+const int d7 = 2;         //Screen d7 pin
+const int buttonPin = A5; //Press button pin
+const int buzzerPin = 8;  //Buzzer pin
+const int pingPin = 13;   //Ultrasonic sensor ping pin
+const int echoPin = A4;   //Ultrasonic sensor echo pin
+const int led1Pin = 6;    //LED 1 pin
+const int led2Pin = 7;    //LED 2 pin
+const int led3Pin = 10;   //LED 2 pin
+const int led4Pin = 9;    //LED 4 pin
+#define TX_PIN 1          //Printer tx pin
+#define RX_PIN 0          //Printer rx pin
 
 //Global constants
-const int tempo = 140; // change this to make the song slower or faster
+const int tempo = 140;    //Tempo of the music played: change this to make the song slower or faster
+const int thisIsLongEnough = 500; //Determines in ms what is a long press on the button
 
 //Variables
-int buttonPushCounter = 0;   // counter for the number of button presses
-int buttonState = 0;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
+int buttonPushCounter = 0;   //Counter for the number of button presses
+int buttonState = 0;         //Current state of the button
+int lastButtonState = 0;     //Previous state of the button
 
 //SETUP
 LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 void setup() {
   //LCD
-  lcd.begin(16, 2);  // set up the LCD's number of columns and rows
-  lcd.clear();
+  lcd.begin(16, 2);  //Set up the LCD's number of columns and rows
+  lcd.clear();       //Clear the screen
 
   //LOADING SCREEN
   lcd.print("Eh toi !!!");
@@ -53,20 +58,36 @@ void setup() {
   Serial.println("Compliment generator starting up...");
 
   //PRINTER
-    printer.begin();  
+  //Printer.begin();
+
+  //ULTRASONIC SENSOR
+  pinMode(pingPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  //LEDs
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  pinMode(led3Pin, OUTPUT);
+  pinMode(led4Pin, OUTPUT);
 }
 
 void loop() {
 
+
+
   //TESTS
-  displayCompliment(randomCompliment());
+  //testUltrasonicSensor();
+  //displayCompliment(randomCompliment());
   //displayDummyCompliment();
   //idleScreen();
   //playMusic();
-  //buttonIsPressedLongEnough(3);
+  //buttonIsPressed();
+  //buttonIsPressedLongEnough(thisIsLongEnough);
   //randomCompliment();
   //printCompliment();
-  delay(999999999999);
+  testLEDs();
+  //delay(999999999999);
+
 
 
   //ROUTINE
@@ -77,10 +98,55 @@ void loop() {
     }*/
 }
 
+void testLEDs() {
+  digitalWrite(led1Pin, HIGH);
+  delay(500);
+  digitalWrite(led1Pin, LOW);
+  digitalWrite(led2Pin, HIGH);
+  delay(500);
+  digitalWrite(led2Pin, LOW);
+  digitalWrite(led3Pin, HIGH);
+  delay(500);
+  digitalWrite(led3Pin, LOW);
+  digitalWrite(led4Pin, HIGH);
+  delay(500);
+  digitalWrite(led4Pin, LOW);
+}
+
+void testUltrasonicSensor() {
+  long duration, inches, cm;
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(100);
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
+}
+
+bool buttonIsPressed() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    Serial.println("Button has been pressed");
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 bool buttonIsPressedLongEnough(int thatIsLongEnough) { //Listen for a long press of the main button.
   //thatIsLongEnough is the time required to press the button in seconds.
-  // read the pushbutton input pin
-  buttonState = digitalRead(buttonPin);
+
+  buttonState = digitalRead(buttonPin); // read the pushbutton input pin
 
   // compare the buttonState to its previous state
   if (buttonState != lastButtonState) {
@@ -109,8 +175,11 @@ String randomCompliment() { //Select a compliment (almost) randomly from a list
   };
 
   //Random compliment selector
+  //Review by Olivier:
   int nbCompliments = sizeof(compliments) / 6;
+  //int nbCompliments = sizeof(compliments) / sizeof(String);
   int numberSelected = random(0, nbCompliments);
+
 
   //Verbose
   Serial.print("Selected compliment: #");
@@ -254,15 +323,15 @@ void displayCompliment(String selectedCompliment) { //Write a random compliment
   Serial.println(string2);
 
   m = 0;
-  
-    lcd.clear();
-    while (m < 10) {
-      lcd.print(string1);
-      lcd.setCursor(0, 1);
-      lcd.print(string2);
-      delay(1000);
-      m++;
-    }
+
+  lcd.clear();
+  while (m < 10) {
+    lcd.print(string1);
+    lcd.setCursor(0, 1);
+    lcd.print(string2);
+    delay(1000);
+    m++;
+  }
 
 }
 
@@ -334,17 +403,15 @@ void playMusic() { //Play a gratifying music to cheer you up!
     }
 
     // we only play the note for 90% of the duration, leaving 10% as a pause
-    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+    tone(buzzerPin, melody[thisNote], noteDuration * 0.9);
 
     // Wait for the specief duration before playing the next note.
     delay(noteDuration);
 
     // stop the waveform generation before the next note.
-    noTone(buzzer);
+    noTone(buzzerPin);
   }
-
-void printCompliment(){
-  
 }
 
+void printCompliment() {
 }
