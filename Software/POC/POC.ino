88a2d29f9cea86b50d2a7339faaadea1effc4cc2
nhippert <nhippert@gmail.com>
2021-02-20 23:21:59 +0100

    Fixes + bigger list of compliments

--- Software/POC/POC.ino
+++ Software/POC/POC.ino
@@ -91,20 +91,25 @@ void loop() {
   //buttonIsPressed();
   //buttonIsPressedLongEnough(thisIsLongEnough);
   //randomCompliment();
-  //printCompliment();
-  //testPrinter();
-  //energySaver(1);
 
 
-  //ROUTINE
-  /*energySaver(1);
+  //ROUTINES
+  //energySavingRoutine();
+  //testRoutine();
+  normalRoutine();
+  
+}
+
+void normalRoutine(){
     idleScreen();
-    if (buttonIsPressedLongEnough(3000)) {
+    if (buttonIsPressedLongEnough(thisIsLongEnough)) {
     displayCompliment(randomCompliment());
     playMusic();
     idleScreen();
-    }*/
-  /*
+    }
+}
+
+void energySavingRoutine(){
     idleScreen();
     //Defines the times at wich the system will go asleep
     timeToFallAsleep = millis() + timeBeforeSavingMode;
@@ -125,7 +130,7 @@ void loop() {
         Serial.print(currentValue);
         Serial.print(" - Previous:");
         Serial.println(previousValue);*/
-  /*  delay(100);
+    delay(100);
     previousValue = currentValue;
     }
 
@@ -149,7 +154,11 @@ void loop() {
       movementDetected = true;
     }
     delay(100);
-    }*/
+    }
+}
+
+void testRoutine(){
+
 }
 
 void routine() {
@@ -276,24 +285,76 @@ breakout:
 String randomCompliment() { //Select a compliment (almost) randomly from a list
   //List of compliments
   String compliments[] = {
-    "Tu fais du bien comme un kebab a 5h du mat",
-    "Tu es beau comme un jour ferie un lundi",
+      "Tu fais du bien comme un kebab a 5h du mat",
+      "Tu es beau comme un jour ferie un lundi",
+      "Sans toi, je suis comme Jul sans vocodeur : rien",
+      "C'est l'inexpugnable arrogance de votre beauté qui m'asperge.",
+      "T'es aussi rare qu'une potatoe dans un paquet de frites"
+      
+    /*Too long list
+     * 
+     * "Tu es beau comme un camion",
+    "Tu fais du bien comme un kebab à 5h du mat",
+    "Tu es beau comme un jour férié un lundi",
+    "Tu es le soleil de mon cœur et je suis bien bronzé",
+    "Si tu n'existais pas, je piquerais quand même pas tes frites",
+    "Ta peau est tellement douce que je pourrai m'en faire un dessus de lit",
+    "T'as le teint rose comme un petit jambon ",
+    "Tes yeux brillent comme la lunette de mes WC que je viens de laver grâce à Canard WC 3 en 1, 50% de promo au rayon surgelés",
+    "Sur une île déserte, je serais bien embêté de devoir te manger",
+    "Tes yeux vont super bien avec la couleur de mon slip",
+    "Quand j'entends ta voix, j'ai le cérumen qui fond",
+    "Tu brilles tellement qu'on dirait du strass",
+    "J'aime bien tes doigts de pieds",
+    "J'ai envie d'aller voir Les Tuche 3 avec toi (et ma mère)",
+    "Je te laisserai la dernière part de pizza",
+    "Si je devais choisir entre le rayon alcool de mon supermarché et toi, je choisirais toi (mais on irait quand même au rayon alcool)",
+    "T'es aussi douce que du PQ ultra-moelleux",
+    "Pour toi j'arrêterais de couper mes spaghettis",
+    "Si on avait une voiture qui roulerait à l'amitié, on irait au bout du monde toi et moi",
+    "C'est pas possible d'être aussi beau, t'es forcément une fake news",
+    "Si t'étais un chien, tu serais un labrador ",
+    "Tu remplis mon coeur comme un spam remplit ma boite mail",
+    "Tes cheveux sont des champs de houblon auxquels on n'oserait pas toucher pour faire de la bière",
+    "Je suis prêt à graver ton image à l'encre noire sous mes paupières",
+    "Tu sens bon comme un paquet de fraise tagada",
     "Sans toi, je suis comme Jul sans vocodeur : rien",
+    "Ton père a volé le respect pour te le donner",
+    "T'es beau comme un coup droit de Roger Federer",
+    "J'adore que tu me ronfles dans l'oreille",
+    "Si on change 5 lettres à ton nom, ça fait je t'aime",
+    "Tu me fais autant rêver qu'un menu Golden ",
+    "Tu es le numéro 1 du Top 100 de mon coeur",
+    "T'es aussi rare qu'une potatoe dans un paquet de frites",
+    "T'es mignon, mais t'es un tout petit breton",
+    "T'es tellement intelligent que tu pourrais jouer dans un biopic sur Einstein",
+    "Sur ma vie t'es grave charmante",
+    "J'aimerais être la chaise sur laquelle tu t'assois ",
+    "J'ai envie de m'allonger près de toi même s'il n'est pas l'heure de la sieste",
+    "Je préfère presque passer du temps avec toi plutôt qu'avec mon chat",
+    "Tu es aussi belle qu'Aphrodite et t'as un meilleur prénom",
+    "T'es beaucoup moins con que t'en as l'air",
+    "T'es un tableau de Picasso sans le côté zarbi des formes",
+    "Quand tu chantes, j'oublie, j'ai plus le moindre souci, j'ai le mal qui fuit, tu donnes un son à ma vie",
+    "Je t'aime plus que tes fringues",
+    "Ton regard est tellement profond que j'ai vu le Commandant Cousteau en sortir",
+    "De dos t'es ouf",
+    "Quand je vois ta mère, j'ai envie de passer les 20 prochaines années avec toi",
+    "Même ton filet de bave nocturne est mignon",
+    "J'aimerais que tu sois une tranche de pain, et que je sois un grille-pain",
+    "Si uN gRaIn dE SabLe SigNifaIt L'aMouR, jE t'OffRirAis lE sAhaRa"*/
   };
 
   //Random compliment selector
-  //Review by Olivier:
   int nbCompliments = sizeof(compliments) / 6;
-  //int nbCompliments = sizeof(compliments) / sizeof(String);
   int numberSelected = random(0, nbCompliments);
-
 
   //Verbose
   Serial.print("Selected compliment: #");
   Serial.print(numberSelected);
   Serial.print(" - ");
   Serial.println(compliments[numberSelected]);
-
+  delay(50);
   return compliments[numberSelected];
 }
 
@@ -317,7 +378,7 @@ void displayCompliment(String selectedCompliment) {
     //This is where things get tricky, because both rows will be scrolled. But we want row 1 to remain stationary!
     lcd.setCursor(j - 16, 0); //Set the cursor to the first character space on the first row [visually].
     // cursor space (0,0) has been scrolled off-screen!
-    lcd.print("Eh bien...."); //Re-print the row 1 message.
+    lcd.print("Eh bien..."); //Re-print the row 1 message.
     lcd.setCursor(j + 1, 1); //Set the cursor one character space to the right of the last printed character on row 2.
     //  Which is visually one character space off-screen, in preparation for the next iteration.
     delay(350); //delay for 0.4 seconds so the user has time to read.
@@ -325,7 +386,8 @@ void displayCompliment(String selectedCompliment) {
   delay(1500);
 }
 
-void ArchivedDisplayCompliment(String selectedCompliment) { //Write a random compliment
+/* Previous version of the compliment display
+  void ArchivedDisplayCompliment(String selectedCompliment) { //Write a random compliment
   String words[100];
   int previousIndexOf = 0;
   int k = 0;
@@ -340,14 +402,14 @@ void ArchivedDisplayCompliment(String selectedCompliment) { //Write a random com
   words[k] = selectedCompliment.substring(previousIndexOf, selectedCompliment.indexOf(' ', previousIndexOf));
 
   //Verbose: Write the words array
-  /*k = 0;
+  k = 0;
     Serial.println("Word array:");
     while (k < 100) {
     Serial.print(k);
     Serial.print(" ");
     Serial.println(words[k]);
     k++;
-    }*/
+    }
 
   //Setup the Lines array
   int Lines[10][10];
@@ -363,7 +425,7 @@ void ArchivedDisplayCompliment(String selectedCompliment) { //Write a random com
   }
 
   //Verbose: Write the Lines array
-  /* m = 0;
+   m = 0;
      n = 0;
      while (m < 10) {
      Serial.print("Line ");
@@ -377,7 +439,7 @@ void ArchivedDisplayCompliment(String selectedCompliment) { //Write a random com
      Serial.println();
      m++;
      n = 0;
-     }*/
+     }
 
   //Fills the Lines array with the words number
   m = 0;
@@ -413,7 +475,7 @@ void ArchivedDisplayCompliment(String selectedCompliment) { //Write a random com
      Serial.println();
      m++;
      n = 0;
-    }*/
+    }
 
   //Display of the compliment
   String string1 = "";
@@ -467,7 +529,7 @@ void ArchivedDisplayCompliment(String selectedCompliment) { //Write a random com
     m++;
   }
 
-}
+  }*/
 
 void displayDummyCompliment() { //For POC purposes only, display an unique static compliment
   //Dummy static compliment
@@ -556,10 +618,6 @@ int readUltrasonicSensor() {
   delayMicroseconds(10);
   digitalWrite(pingPin, LOW);
   return pulseIn(echoPin, HIGH);
-}
-
-boolean energySaver(long timeBeforeSavingMode) { //Allow theCompliment Generator to go into energy saving mode (v1 feature).
-
 }
 
 void printCompliment(String selectedCompliment) {//Print the compliment given in parameter (v2 feature).
